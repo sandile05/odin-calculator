d921f740e84c060cd4fd1dfafa632436c5af032f
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-04-12 09:28:02 +0200

    created DOM elements

---
+++ main.js
@@ -0,0 +1,16 @@
+let operator = '';
+let previousValue = '';
+let currentValue = '';
+
+document.addEventListener("DOMContentLoaded", function() {
+    //store all html componets in js
+    let clear = document.querySelector('#clear-btn');
+    let equal = document.querySelector('.equal');
+    let decimal = document.querySelector('.decimal');
+
+    let numbers = document.querySelectorAll('.number');
+    let operators = document.querySelectorAll('.operator');
+
+    let previousScreen = document.querySelector('.previous');
+    let currentScreen = document.querySelector('.current');
+})
